8063fca7b232406212fd85f47c658a0f74a841c0
madhawan17 <aryamadhawan@gmail.ingit config --global user.email aryamadhawan@gmail.com>
2025-08-29 22:46:06 +0530

    route handlers

--- src/app.js
+++ src/app.js
@@ -2,24 +2,28 @@ const express = require('express');
 
 const app = express();
 
-// this will only handle get call 
-app.get("/user",(req, res) => {
-    res.send({ firstname: "Madhawan", lastname: "Arya"});
-});
-
-app.post("/user",(req, res) => {
-    res.send("data succesfully saved to database");
-});
-
-app.delete("/user",(req, res) => {
-    res.send("Deleted succesfully");
-});
-
-// This will match all http method API calls 
-
-app.use("/test",(req, res) => {
-    res.send("Hello from the server!");
-});
+app.use(
+    "/user", (req, res, next) => {
+        console.log("Handling the route user!!");
+        // res.send("Response!!");
+        next();
+    },
+    (req, res,next) => {
+        console.log("Handling the route user 2!!");
+        // res.send("2nd Response!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 3!!");
+        // res.send("3rd Response!!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling the route user 4!!");
+         res.send("4th Response!!");
+        
+    }
+);
 
 app.listen(7777, () =>{
     console.log('Server is succesfully connected to port 7777');
