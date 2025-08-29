1d0cb0e0766548bd9456b13466deb4e7899bbe15
madhawan17 <aryamadhawan@gmail.ingit config --global user.email aryamadhawan@gmail.com>
2025-08-29 22:58:41 +0530

    remove 3rd response

--- src/app.js
+++ src/app.js
@@ -13,11 +13,8 @@ app.use(
         // res.send("2nd Response!!");
         next();
     },
-    (req, res, next) => {
-        console.log("Handling the route user 3!!");
-        // res.send("3rd Response!!");
-        next();
-    },
+    
+
     (req, res, next) => {
         console.log("Handling the route user 4!!");
          res.send("4th Response!!");
