c9498a601ef5c617967a58cb6952ad008c95787d
madhawan17 <aryamadhawan@gmail.com>
2025-09-03 23:45:09 +0530

    data sanitization in api

--- src/app.js
+++ src/app.js
@@ -65,17 +65,37 @@ app.delete("/user", async (req, res) => {
 } );
 
 // Update data of the user 
-app.patch("/user", async (req,res) => {
-    const userId = req.body.userId;
+app.patch("/user/:userId", async (req,res) => {
+    const userId = req.params?.userId;
     const data = req.body;
 
+
     try {
-         const user = await User.findByIdAndUpdate({_id: userId}, data, {returnDocument: "before"} );
+        const ALLOWED_UPDATES = [
+        "photourl", "about", "age", "gender", "skills"
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+         ALLOWED_UPDATES.includes(k)
+    );
+
+    if(!isUpdateAllowed) {
+        throw new Error("Updatessss not allowed");
+    }
+
+    if(data?.skills.length > 10) {
+        throw new Error("Skills cannot be more than 10");
+    }
+
+         const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true,
+        } );
          console.log(user);
         res.send ("user updated successfully");
     }
     catch (err) {
-        res.status(400).send("Something went wrong!!")
+        res.status(400).send("Update failed:"  + err.message);
     }
 
 });
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,51 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 3,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
+
     },
     password : {
-        type: String
+        type: String,
+        required: true
     },
     age: {
         type: Number
     },
     gender: {
-        type: String
+        type: String,
+        min: 18,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Not a valid gender");
+            }
+        },
     },
-});
+    photoUrl: {
+        type: String,                                   
+    },
+    about: {
+        type: String,
+        default: "This is the default about of the user!",
+    },
+    skills: {
+        type: [String],
+    },
+},
+{
+    timestamps: true,
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
