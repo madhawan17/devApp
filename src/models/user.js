b9a1c2f92d4f51bf57ea02242a13247a9fcca886
madhawan17 <aryamadhawan@gmail.com>
2025-09-04 00:08:18 +0530

    exploring validation library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,11 +16,20 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
-
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error ("Invalid email address:" + value);
+            }
+        },
     },
     password : {
         type: String,
-        required: true
+        required: true,
+        validate(value) {
+            if(!validator.isStrongPassword(value)) {
+                throw new Error ("Enter a strong pasword:" + value);
+            }
+        },
     },
     age: {
         type: Number
