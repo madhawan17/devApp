76e39aeadb4d24bb5375de0938513226d019b582
madhawan17 <aryamadhawan@gmail.com>
2025-09-08 21:16:08 +0530

    auth

--- apiList.md
+++ apiList.md
@@ -1,22 +1,22 @@
 # Devtinder Apis
 
-authRouter
+## authRouter
 -POST /signup
 -POST /login
 -POST /logout
 
-profileRouter
+## profileRouter
 -GET /profile/view
 -PATCH /profile/password
 -PATCH /profile/edit
 
-connectionRequestRouter
+## connectionRequestRouter
 -POST /request/send/interested/:userId
 -POST /request/send/ignored/:userId
 -POST /request/review/accepted/:requestId
 -POST /request/review/rejected/:requestId
 
-userRouter
+## userRouter
 -GET/user/connections
 -GET/user/requests/received
 -GET/user/feed - Gets you the profile of other users on platform    
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,16 +1,11 @@
-const express = require('express');
-const connectDB = require('./config/database.js');
-const app = express();
+const express = require("express");
+const authRouter = express.Router();
+const User = require('./models/user');
 const bcrypt = require("bcrypt");
-const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");
-const { userAuth} = require("./middleware/auth");
 
-app.use(express.json());
-app.use(cookieParser());
 
-// Signup api
-app.post("/signup", async (req,res) => { 
+
+authRouter.post("/signup", async (req,res) => { 
 try{    
     
 // Validation of data 
@@ -39,8 +34,7 @@ const user = new User({
    }
 });
 
-// Login api 
-app.post("/login", async (req,res) => {
+authRouter.post("/login", async (req,res) => {
 
     try {
        const { emailId, password} = req.body;
@@ -75,39 +69,6 @@ app.post("/login", async (req,res) => {
         res.status(400).send("Error : " + err.message);
         
     }
-})
-
-// profile api 
-app.get("/profile", userAuth, async (req,res) => {
-    try {
-        const user = req.user;
-
-    res.send( user);
-    } catch (err) {
-        res.status(400).send("Error : " + err.message);
-
-    }    
 });
 
-app.post("/sendConnectionRequest",userAuth, async (req,res) => {
-    const user = req.user;
-    // Sending the connection request
-    console.log("sending the connection request");
-
-    res.send(user.firstName + " sent the connection request"); 
-
-});
-
-connectDB()
-.then(()=> {
-console.log("database connected");
-app.listen(7777, () =>{
-    console.log('Server is succesfully connected to port 7777');
-
-});
-})
-.catch(err=>{
-console.log("database not connected");
-   
-});
-
+module.exports = authRouter;
